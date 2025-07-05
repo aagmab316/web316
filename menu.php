7569e318e28e361a319ded69caa4a7ae2a868d26
aagmab316 <amrgamal316@gmail.com>
2025-07-05 17:56:45 -0400

    t3

---
+++ menu.php
@@ -0,0 +1,171 @@
+ 
+     <nav class="fix-menu">
+
+      <a hre="index.php"><div class="resLogo"><img src="images/logo.png" /></div></a>
+        <div id="dl-menu" class="dl-menuwrapper">
+            <button class="dl-trigger">Open Menu</button>
+
+            <ul class="dl-menu resPM">
+                <li class="lgoLI">
+               <a href="index.php"><div class="logo1"></div></a>
+                </li>
+                <!-- <li>
+                    <a class="scroll" href="index.php">home</a>
+                </li> -->
+                <li>
+                    <a  href="#about">about</a>
+                </li>
+                <li class="dwnarow">
+                    <a id="primary1" href="#">Cybersecurity Services</a>
+                    <ul class="customDropMenu" id="primary2">
+                    <li><a href="Cybersecurity-posture-assessment.php">- Posture assessment</a></li>
+                            <li><a href="#">- Regulations & legalization</a></li>
+                            <li><a href="incident-response-digital-forensic.php">- Incident response and digital forensic</a></li>
+                            <li><a href="governance-compliance.php">- Governance & compliance</a></li>
+                            <li><a href="#">- Audit</a></li>
+                            <li><a href="cyber-crime-awarness-2.php">- Cybercrime Awareness </a></li>
+                            <li><a href="governance-compliance.php">- Digital Forensic</a></li>
+                            <li><a href="managed-security-service-provider(MSSP).php">- Managed security service provider(MSSP)	</a></li>
+                            <li><a href="#">- Network and endpoint security Services</a></li>
+                            <li><a href="cyber-security-solutions-integration-services.php">- IT system integration</a></li>
+                    </ul>
+                </li>
+                <li class="dwnarow">
+                    <a id="primary3" href="#">Software Services</a>
+                    <ul class="customDropMenu" id="primary4">
+                    <li><a href="sbr-startup-full-consultancy-service.php">- Solution Startup Consultancy</a></li>
+                            <li><a href="software-development.php">- Software Development</a></li>
+                            <li><a href="application-support-and-maintenance.php">- Application support and maintenance</a></li>
+
+                    </ul>
+                </li>
+                <li class="dwnarow">
+                    <a href="#" id="primary5">Cybercrime Awareness</a>
+                    <ul class="customDropMenu" id="primary6">
+                        <li><a href="#"> - Public Awareness</a></li>
+                        <li><a href="#"> - Corporate Awarness</a></li>
+
+                    </ul>
+                </li>
+                <li class="dwnarow">
+                    <a  href="#" id="primary7">knowledge Center</a>
+                    <ul class="customDropMenu" id="primary8">
+                        <li><a href="visit-internet-security-center.php"> - Visit Internet Security Center</a></li>
+                        <li><a href="social-media-privacy-tips.php"> - Social Media Privacy Tips</a></li>
+                        <li><a href="#"> - Internet Safety for All</a></li>
+                        <li><a href="cybersecurity-hygiene-and-faqs.php"> - Cybersecurity Hygiene and FAQs</a></li>
+
+                    </ul>
+                </li>
+                <!-- <li>
+                    <a class="scroll" href="#work">work</a>
+
+                </li>
+                <li>
+                    <a class="scroll" href="#services">services</a>
+                </li> -->
+                <li>
+                    <a class="scroll" href="#contact">contact us</a>
+                </li>
+                <li>
+                    <div class="social-home">
+                        <ul class="list-social">
+                            <li class="icon-soc">
+                                <a href="#">&#xf099;</a>
+                            </li>
+                            <li class="icon-soc">
+                                <a href="#">&#xf173;</a>
+                            </li>
+                            <li class="icon-soc">
+                                <a href="#">&#xf17d;</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+
+    <nav id="menu-wrap" class="menu-back cbp-af-header">
+        <div class="container containerM">
+            <div class=" ">
+            <a href="index.php">   <div class="logo"></div></a>
+            </div>
+            <div class=" ">
+                <div class="social-home">
+                    <ul class="list-social">
+                        <li class="icon-soc">
+                            <a href="#">&#xf099;</a>
+                        </li>
+                        <li class="icon-soc">
+                            <a href="#">&#xf173;</a>
+                        </li>
+                        <li class="icon-soc">
+                            <a href="#">&#xf17d;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="nav navM">
+                <ul class="slimmenu">
+                    <!-- <li>
+                        <a class="scrollm" href="index.php">home</a>
+                    </li> -->
+                    <li>
+                        <a href="cybersecurity-services.php">Cybersecurity Services</a>
+                        <ul class="customDropMenu">
+                            <li><a href="Cybersecurity-posture-assessment.php">Posture assessment</a></li>
+                            <li><a href="#">Regulations & legalization</a></li>
+                            <li><a href="incident-response-digital-forensic.php">Incident response and digital forensic</a></li>
+                            <li><a href="governance-compliance.php">Governance & compliance</a></li>
+                            <li><a href="#">Audit</a></li>
+                            <li><a href="cyber-crime-awarness-2.php">Cybercrime Awareness </a></li>
+                            <li><a href="governance-compliance.php">Digital Forensic</a></li>
+                            <li><a href="managed-security-service-provider(MSSP).php">Managed security service provider(MSSP)	</a></li>
+                            <li><a href="#">Network and endpoint security Services</a></li>
+                            <li><a href="cyber-security-solutions-integration-services.php">IT system integration</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="scroll" href="software-services.php">Software Services</a>
+                        <ul class="customDropMenu">
+                            <li><a href="sbr-startup-full-consultancy-service.php">Solution Startup Consultancy</a></li>
+                            <li><a href="software-development.php">Software Development</a></li>
+                            <li><a href="application-support-and-maintenance.php">Application support and maintenance</a></li>
+
+                        </ul>
+                    </li>
+                    <li>
+                        <a   href="cyber-crime-awarness-2.php">Cybercrime Awareness</a>
+                        <ul class="customDropMenu">
+                            <li><a href="#">Public Awareness</a></li>
+                            <li><a href="#">Corporate Awarness</a></li>
+
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="knowledge-centre.php">knowledge Center</a>
+                        <ul class="customDropMenu">
+                            <li><a href="visit-internet-security-center.php">Visit Internet Security Center</a></li>
+                            <li><a href="social-media-privacy-tips.php">Social Media Privacy Tips</a></li>
+                            <li><a href="#">Internet Safety for All</a></li>
+                            <li><a href="cybersecurity-hygiene-and-faqs.php">Cybersecurity Hygiene and FAQs</a></li>
+
+                        </ul>
+                    </li>
+
+
+                    <li>
+                        <a class="scroll" href="#contact">contact us</a>
+
+                    </li>
+                </ul>
+            </div>
+
+
+
+        </div>
+    </nav>
+    
